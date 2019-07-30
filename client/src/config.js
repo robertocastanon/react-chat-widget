da6085f808c74f221c5afd3a989d9a4f7997a711
Roberto Castanon <robertocastanon925@gmail.com>
2019-07-30 14:28:38 -0700

    finished chat app

---
+++ client/src/config.js
@@ -0,0 +1,6 @@
+const config = {
+  appID: '5888df2b837050',
+  agentUID: 'ecommerce-agent'
+}
+
+export default config
--- server.js
+++ server.js
@@ -1,16 +1,81 @@
+require('dotenv').config({ path: '.env' });
+
 const express = require('express');
 const axios = require('axios');
 const app = express();
 
-// enter CometChat Pro configurations here
-const appID = '{appID}';
-const apiKey = '{apiKey}';
-const agentUID = '{agentUID}';
+const appID = '5888df2b837050'
+const apiKey = process.env.API_KEY
+const agentUID = 'ecommerce-agent'
 
 const url = 'https://api.cometchat.com/v1';
 
 const headers = {
   'Content-Type': 'application/json',
   appid: appID,
-  apikey: apiKey,
+  apikey: apiKey
 };
+
+app.get('/api/create', (req, res) => {
+  const data = {
+    uid: new Date().getTime(),
+    name: 'customer',
+  };
+  axios
+    .post(`${url}/users`, JSON.stringify(data), {
+      headers,
+    })
+    .then(response => {
+      requestAuthToken(response.data.data.uid)
+        .then(token => {
+          console.log('Success:' + JSON.stringify(token));
+          res.json(token);
+        })
+        .catch(error => console.error('Error:', error));
+    })
+    .catch(error => console.error('Error:', error));
+});
+
+app.get('/api/auth', (req, res) => {
+  const uid = req.query.uid;
+  requestAuthToken(uid)
+    .then(token => {
+      console.log('Success:' + JSON.stringify(token));
+      res.json(token);
+    })
+    .catch(error => console.error('Error:', error));
+});
+
+const requestAuthToken = uid => {
+  return new Promise((resolve, reject) => {
+    axios
+      .post(`${url}/users/${uid}/auth_tokens`, null, {
+        headers,
+      })
+      .then(response => {
+        console.log('New Auth Token:', response.data);
+        resolve(response.data.data);
+      })
+      .catch(error => reject(error));
+  });
+};
+
+app.get('/api/users', (req, res) => {
+  axios
+    .get(`${url}/users`, {
+      headers,
+    })
+    .then(response => {
+      const { data } = response.data;
+      const filterAgentData = data.filter(data => {
+        return data.uid !== agentUID;
+      });
+      res.json(filterAgentData);
+    })
+    .catch(error => console.error('Error:', error));
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+})
